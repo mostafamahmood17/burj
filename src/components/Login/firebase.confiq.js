77b217cc0e7440134bd7a526e1855b0f676cfe38
mostafamahmood17 <taat.heritage@gmail.com>
2020-09-12 01:10:08 -0700

    wow

---
+++ src/components/Login/firebase.confiq.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAo9jmQrimH1fNlHCR6XAOdfcIONeWGe6k",
+    authDomain: "burj-al-arab-hotel.firebaseapp.com",
+    databaseURL: "https://burj-al-arab-hotel.firebaseio.com",
+    projectId: "burj-al-arab-hotel",
+    storageBucket: "burj-al-arab-hotel.appspot.com",
+    messagingSenderId: "1069740910075",
+    appId: "1:1069740910075:web:188c48b80a9ead9f98575e"
+  };
+
+  export default firebaseConfig;
